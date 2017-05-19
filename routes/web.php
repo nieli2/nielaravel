7c09213415482b31de524ae726d06a2a4b57bce8
nieli2 <nieli2@users.noreply.github.com>
2017-05-19 22:24:56 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,21 @@ Route::get('/', function () {
 });
 
 Route::get('angular', function () {
-	$data = DB::table('tasks')->get();
+	$data['data'] = DB::table('tasks')->get();
+	$data['_token'] = [['id'=>csrf_token(),'name'=>'niel']]; // this is not yet done (data should be displayed on angular)
 	return json_encode($data);
     //return view('angular');
+});
+Route::get('angular-insert', function () {
+	echo '<form method="post" action="/angular-insert">';
+	echo csrf_field();
+	echo "<input name='name'>";
+	echo '</form>';
+});
+Route::post('angular-insert', function () {
+	DB::table('tasks')->insert(
+		['name' => Request('name')]
+	);
+
+	return redirect('/angular');
 });
